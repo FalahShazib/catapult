2fb8d3148a0b478577d28bebaccda8b85833abbf
uwvr <uwvr>
2019-04-18 18:28:50 -0400

    first commit

---
+++ Assets/DetectTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DetectTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("looking at cube");
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("stopped looking at cube");
+    }
+
+}
---
+++ Assets/GrabStars.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabStars : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //if (collision.tag == "star")
+        //{
+            // Object.Destroy(collision.gameObject);
+            Debug.Log("caught a star");
+        //}
+    }
+}
